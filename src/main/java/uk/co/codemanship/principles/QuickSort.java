0dbffb49f247a37a31969d379c237b4e8bca60e2
Will Price <will.price94@gmail.com>
2018-06-10 16:43:28 +0100

    Try to shorten good version of code

--- src/main/java/uk/co/codemanship/principles/QuickSort.java
+++ src/main/java/uk/co/codemanship/principles/QuickSort.java
@@ -1,60 +1,44 @@
 package uk.co.codemanship.principles;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class QuickSort {
     public int[] sortAsc(int[] input) {
         return quicksort(input, 0, input.length - 1);
     }
 
-    private int partition(int[] array, int left, int right) {
-        int i = left, j = right;
-        int pivot = array[(left + right) / 2];
-
-        i = pivotElements(array, i, j, pivot);
-
-        return i;
-
-    }
-
-    private int pivotElements(int[] array, int i, int j, int pivot) {
-        while (i <= j) {
-            while (array[i] < pivot)
-                i++;
-            while (array[j] > pivot)
-                j--;
-            if (i <= j) {
-                swap(array, i, j);
-                i++;
-                j--;
-            }
-        };
-        return i;
-    }
-
-    private void swap(int[] array, int left, int right) {
-        int old_left = array[left];
-        array[left] = array[right];
-        array[right] = old_left;
-    }
-
     private int[] quicksort(int[] input, int left, int right) {
-        int index = partition(input, left, right);
-        sortLeft(input, left, index);
-        sortRight(input, right, index);
-        return input;
-    }
+        int lowerBound = left, upperBound = right;
+        int pivot = input[(left + right) / 2];
 
-    private void sortRight(int[] input, int right, int index) {
-        if (index < right){
-            quicksort(input, index, right);
-        }
-    }
-
-    private void sortLeft(int[] input, int left, int index) {
+        int index = partition(input, pivot, lowerBound, upperBound);
         if (left < index - 1){
             quicksort(input, left, index - 1);
         }
+        if (index < right - 1){
+            quicksort(input, index, right);
+        }
+        return input;
     }
+
+    private int partition(int[] input, int pivot, int lowerBound, int upperBound) {
+        while (lowerBound < upperBound) {
+            while (input[lowerBound] < pivot)
+                lowerBound++;
+            while (input[upperBound] > pivot)
+                upperBound--;
+            if (lowerBound < upperBound) {
+                swap(input, lowerBound, upperBound);
+                lowerBound++;
+                upperBound--;
+            }
+        }
+
+        return lowerBound;
+    }
+
+    private void swap(int[] input, int i, int j) {
+        int old_left = input[i];
+        input[i] = input[j];
+        input[j] = old_left;
+    }
+
 }
